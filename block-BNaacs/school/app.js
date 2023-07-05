3e651f8c2ca85017399f18942a133912782290d8
anuj sachan <anujsachan98@gmail.com>
2023-07-05 13:59:05 +0530

    changes

--- block-BNaacs/school/app.js
+++ block-BNaacs/school/app.js
@@ -1,15 +1,15 @@
- var express  = require('express');
+var express = require("express");
+ var path = require('path');
 
-  var app = express();
-  // middlewere
+var app = express();
 
-  app.set('view' , 'ejs');
-  app.set('views' , path.join(__dirname , 'views'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-  app.get('/', (req, res) =>{
-    res.render('index.ejs')
-  })
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-  app.listen(3000 ,  () =>{
-    console.log('server is listening on port 3k');
-  })
+app.listen(3000, () => {
+  console.log("server is listening on port 3k");
+});
