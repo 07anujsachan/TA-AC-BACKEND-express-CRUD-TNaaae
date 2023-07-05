ce08a6553ecc5e7884151c90635a87e87129d895
anuj sachan <anujsachan98@gmail.com>
2023-07-05 12:30:01 +0530

    Views in Express

---
+++ block-BNaacs/school/app.js
@@ -0,0 +1,14 @@
+ var express  = require('express');
+
+  var app = express();
+
+  app.set('view' , 'ejs');
+  app.set('views' , path.join(__dirname , 'views'))
+
+  app.get('/', (req, res) =>{
+    res.render('index.ejs')
+  })
+
+  app.listen(3000 ,  () =>{
+    console.log('server is listening on port 3k');
+  })
